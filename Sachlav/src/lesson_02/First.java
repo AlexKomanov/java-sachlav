de4e90c711089c3eba9e14a73538f7413b83b3ac
AlexKomanov <akomanov88@gmail.com>
2022-06-20 19:49:56 +0300

    Lesson 2 code

--- Sachlav/src/lesson_02/First.java
+++ Sachlav/src/lesson_02/First.java
@@ -1,6 +1,8 @@
+package lesson_02;
+
 public class First {
     public static void main(String[] args) {
-        
+
 
         System.out.println("Hello World!");
         System.out.println("    Hello World!");
---
+++ Sachlav/src/lesson_02/Variables.java
@@ -0,0 +1,44 @@
+package lesson_02;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        // One line initialization
+        byte myByte = 2;
+
+        // Different letters of the same name
+        byte myBytE;
+        byte MYBYTE;
+
+        // Two lines initialization
+        byte myByte_2;
+        myByte_2 = 3;
+
+        // myByte = 320; Triggers Error
+
+        short myShort = 23423;
+
+        int myInt = 3453454;
+
+        long myLong = 12312321312312312L;
+
+        float myFloat = 5.56f;
+
+        double myDouble = 5.678;
+
+        boolean myTrue = true;
+
+        boolean myFalse = false;
+
+        char myChar = 'd';
+        char myChar2 = '9';
+        char myChar3 = '\u03A8';
+
+        String myString = "Alex";
+
+        System.out.println("myChar3 = " + myChar3);
+        System.out.println("myInt = " + myInt);
+
+
+    }
+}
