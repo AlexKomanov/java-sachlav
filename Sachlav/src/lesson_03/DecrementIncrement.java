31b8c5ff2a42766837927f9860d17ccefce4649b
AlexKomanov <akomanov88@gmail.com>
2022-06-22 18:25:03 +0300

    Increment Decrement

---
+++ Sachlav/src/lesson_03/DecrementIncrement.java
@@ -0,0 +1,21 @@
+package lesson_03;
+
+public class DecrementIncrement {
+    public static void main(String[] args) {
+
+        int a = 5;
+        System.out.println("a = " + a);
+        a = a + 1;
+        System.out.println("a = " + a);
+        a+=1;
+        System.out.println("a = " + a);
+        a++;
+        System.out.println("a = " + a);
+        a--;
+        System.out.println("a = " + a);
+        System.out.println("a = " + a);
+        ++a;
+        System.out.println("a = " + a);
+
+    }
+}
