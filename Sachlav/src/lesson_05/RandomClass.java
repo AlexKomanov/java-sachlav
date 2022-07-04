05ba478d40c79b6e9f76809ec1ce350725982ff9
AlexKomanov <akomanov88@gmail.com>
2022-07-04 19:03:24 +0300

    Lesson 5 arrays commit 1

---
+++ Sachlav/src/lesson_05/RandomClass.java
@@ -0,0 +1,27 @@
+package lesson_05;
+
+import java.util.Random;
+
+public class RandomClass {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        int randomNumber = random.nextInt(300);
+        System.out.println("randomNumber = " + randomNumber);
+
+        double randomDouble = random.nextDouble();
+        System.out.println("randomDouble = " + randomDouble);
+
+        int[] array = new int[100];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt();
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+
+        }
+
+    }
+}
